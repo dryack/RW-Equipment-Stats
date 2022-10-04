e1be2ddab4dc6ed9489271cf939a984c84267935
dryack <git.lamashtu@gmail.com>
2022-10-04 16:20:31 -0700

    Update rw-equip-stats.user.js

--- rw-equip-stats.user.js
+++ rw-equip-stats.user.js
@@ -3,7 +3,7 @@
 // @namespace   lamashtu.rw_equipment_stats
 // @match       https://www.torn.com/item.php
 // @match       https://www.torn.com/factions.php?step=your#/tab=armoury
-// @updateURL   https://github.com/dryack/RW-Equipment-Stats/rw-equip-stats.user.js
+// @updateURL   https://github.com/dryack/RW-Equipment-Stats/blob/main/rw-equip-stats.user.js
 // @version     1.2
 // @author      lamashtu
 // @description Track RQ equipment stats in Torn's UI
